82ca66cb36bfbe71210680b7841e8d11b4e8dcbb
reiniscimurs <33617541+reiniscimurs@users.noreply.github.com>
2021-10-26 10:08:23 +0900

    Add files via upload

---
+++ TD3/velodyne_env.py
@@ -0,0 +1,509 @@
+import rospy
+import subprocess
+from os import path
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+from numpy import inf
+import numpy as np
+import random
+import math
+from gazebo_msgs.msg import ModelState
+from squaternion import Quaternion
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan, PointCloud2
+import sensor_msgs.point_cloud2 as pc2
+from nav_msgs.msg import Odometry
+from std_srvs.srv import Empty
+import os
+import time
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+def check_pos(x, y):
+    goalOK = True
+
+    if -3.8 > x > -6.2 and 6.2 > y > 3.8:
+        goalOK = False
+
+    if -1.3 > x > -2.7 and 4.7 > y > -0.2:
+        goalOK = False
+
+    if -0.3 > x > -4.2 and 2.7 > y > 1.3:
+        goalOK = False
+
+    if -0.8 > x > -4.2 and -2.3 > y > -4.2:
+        goalOK = False
+
+    if -1.3 > x > -3.7 and -0.8 > y > -2.7:
+        goalOK = False
+
+    if 4.2 > x > 0.8 and -1.8 > y > -3.2:
+        goalOK = False
+
+    if 4 > x > 2.5 and 0.7 > y > -3.2:
+        goalOK = False
+
+    if 6.2 > x > 3.8 and -3.3 > y > -4.2:
+        goalOK = False
+
+    if 4.2 > x > 1.3 and 3.7 > y > 1.5:
+        goalOK = False
+
+    if -3.0 > x > -7.2 and 0.5 > y > -1.5:
+        goalOK = False
+
+    if x > 4.5 or x < -4.5 or y > 4.5 or y < -4.5:
+        goalOK = False
+    return goalOK
+
+
+def binning(lower_bound, data, quantity):
+    width = round(len(data) / quantity)
+    quantity -= 1
+    bins = []
+    for low in range(lower_bound, lower_bound + quantity * width + 1, width):
+        bins.append(min(data[low:low + width]))
+    return np.array([bins])
+
+
+class GazeboEnv:
+    """Superclass for all Gazebo environments.
+    """
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, launchfile, height, width, nchannels):
+
+        self.odomX = 0
+        self.odomY = 0
+
+        self.goalX = 1
+        self.goalY = 0.0
+
+        self.upper = 5.0
+        self.lower = -5.0
+        self.velodyne_data = np.ones(20) * 10
+
+        self.set_self_state = ModelState()
+        self.set_self_state.model_name = 'r1'
+        self.set_self_state.pose.position.x = 0.
+        self.set_self_state.pose.position.y = 0.
+        self.set_self_state.pose.position.z = 0.
+        self.set_self_state.pose.orientation.x = 0.0
+        self.set_self_state.pose.orientation.y = 0.0
+        self.set_self_state.pose.orientation.z = 0.0
+        self.set_self_state.pose.orientation.w = 1.0
+        self.distOld = math.sqrt(math.pow(self.odomX - self.goalX, 2) + math.pow(self.odomY - self.goalY, 2))
+        self.gaps = [[-1.6, -1.57 + 3.14 / 20]]
+        for m in range(19):
+            self.gaps.append([self.gaps[m][1], self.gaps[m][1] + 3.14 / 20])
+        self.gaps[-1][-1] += 0.03
+
+        # port = os.environ["ROS_PORT_SIM"]
+        # start roscore
+        port = '11311'
+        subprocess.Popen(["roscore", "-p", port])
+
+        print("Roscore launched!")
+
+        # Launch the simulation with the given launchfile name
+        rospy.init_node('gym', anonymous=True)
+        # time.sleep(7)
+        if launchfile.startswith("/"):
+            fullpath = launchfile
+        else:
+            fullpath = os.path.join(os.path.dirname(__file__), "assets", launchfile)
+            # fullpath = os.path.join(os.path.dirname(__file__), "turtlebot", launchfile)
+        if not path.exists(fullpath):
+            raise IOError("File " + fullpath + " does not exist")
+
+        subprocess.Popen(["roslaunch", "-p", port, fullpath])
+        print("Gazebo launched!")
+
+        # time.sleep(3)
+        self.gzclient_pid = 0
+
+        # env = gym.make('MultiRobotScenario-v0')
+
+        self.vel_pub = rospy.Publisher('/r1/cmd_vel', Twist, queue_size=1)
+        self.set_state = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size=10)
+        self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+        self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_world', Empty)
+        topic = 'vis_mark_array'
+        self.publisher = rospy.Publisher(topic, MarkerArray, queue_size=3)
+        topic2 = 'vis_mark_array2'
+        self.publisher2 = rospy.Publisher(topic2, MarkerArray, queue_size=1)
+        topic3 = 'vis_mark_array3'
+        self.publisher3 = rospy.Publisher(topic3, MarkerArray, queue_size=1)
+        topic4 = 'vis_mark_array4'
+        self.publisher4 = rospy.Publisher(topic4, MarkerArray, queue_size=1)
+        self.velodyne = rospy.Subscriber('/velodyne_points', PointCloud2, self.velodyne_callback ,queue_size = 1)
+
+    def velodyne_callback(self, v):
+        data = list(pc2.read_points(v,skip_nans= False, field_names=("x","y","z")))
+        self.velodyne_data = np.ones(20)*10
+        for i in range(len(data)):
+            #print(data[i][2])
+            if data[i][2] > -0.2:
+                dot = data[i][0] * 1 + data[i][1] * 0
+                mag1 = math.sqrt(math.pow(data[i][0], 2) + math.pow(data[i][1], 2))
+                mag2 = math.sqrt(math.pow(1, 2) + math.pow(0, 2))
+                beta = math.acos(dot / (mag1 * mag2)) * np.sign(data[i][1])# * -1
+                dist = math.sqrt(data[i][0]**2 + data[i][1]**2 + data[i][2]**2)
+
+                for j in range(len(self.gaps)):
+                    if self.gaps[j][0] <= beta < self.gaps[j][1]:
+                        self.velodyne_data[j] = min(self.velodyne_data[j],dist)
+                        break
+
+        #print(self.velodyne_data)
+
+    def calculate_observation(self, data):
+        min_range = 0.3
+        min_laser = 2
+        done = False
+        col = False
+
+        for i, item in enumerate(data.ranges):
+            if min_laser > data.ranges[i]:
+                min_laser = data.ranges[i]
+            if (min_range > data.ranges[i] > 0):
+                done = True
+                col = True
+                # print data.ranges[i]
+        # print ("Observation!")
+        return done, col, min_laser
+
+    def step(self, act):
+        target = False
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        # print ("Step!")
+        try:
+            self.unpause()
+        except (rospy.ServiceException) as e:
+            print("/gazebo/unpause_physics service call failed")
+
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('/r1/front_laser/scan', LaserScan, timeout=0.5)
+            except:
+                pass
+
+        laser_state = np.array(data.ranges[:])
+        laser_state[laser_state == inf] = 10
+        laser_state = binning(0, laser_state, 20)
+        v_state = []
+        v_state[:] = self.velodyne_data[:]
+        #print(v_state)
+        #print(laser_state)
+        laser_state = [v_state]
+
+        #print([v_state])
+        #laser_state = laser_state / 10
+
+        done, col, min_laser = self.calculate_observation(data)
+
+        dataOdom = None
+        while dataOdom is None:
+            try:
+                dataOdom = rospy.wait_for_message('/r1/odom', Odometry, timeout=0.5)
+            except:
+                pass
+        time.sleep(0.1)
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            pass
+            # resp_pause = pause.call()
+            self.pause()
+        except (rospy.ServiceException) as e:
+            print("/gazebo/pause_physics service call failed")
+        #time.sleep(1)
+
+        self.odomX = dataOdom.pose.pose.position.x
+        self.odomY = dataOdom.pose.pose.position.y
+        quaternion = Quaternion(
+            dataOdom.pose.pose.orientation.w,
+            dataOdom.pose.pose.orientation.x,
+            dataOdom.pose.pose.orientation.y,
+            dataOdom.pose.pose.orientation.z)
+        euler = quaternion.to_euler(degrees=False)
+        angle = round(euler[2], 4)
+
+        Dist = math.sqrt(math.pow(self.odomX - self.goalX, 2) + math.pow(self.odomY - self.goalY, 2))
+
+        skewX = self.goalX - self.odomX
+        skewY = self.goalY - self.odomY
+
+        dot = skewX * 1 + skewY * 0
+        mag1 = math.sqrt(math.pow(skewX, 2) + math.pow(skewY, 2))
+        mag2 = math.sqrt(math.pow(1, 2) + math.pow(0, 2))
+        beta = math.acos(dot / (mag1 * mag2))
+
+        if skewY < 0:
+            if skewX < 0:
+                beta = -beta
+            else:
+                beta = 0 - beta
+        beta2 = (beta - angle)
+
+        if beta2 > np.pi:
+            beta2 = np.pi - beta2
+            beta2 = -np.pi - beta2
+        if beta2 < -np.pi:
+            beta2 = -np.pi - beta2
+            beta2 = np.pi - beta2
+
+        vel_cmd = Twist()
+        vel_cmd.linear.x = act[0]
+        vel_cmd.angular.z = act[1]
+
+        self.vel_pub.publish(vel_cmd)
+
+        markerArray = MarkerArray()
+        marker = Marker()
+        marker.header.frame_id = "base_link"
+        marker.type = marker.CYLINDER
+        marker.action = marker.ADD
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.01
+        marker.color.a = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.pose.orientation.w = 1.0
+        marker.pose.position.x = self.goalX
+        marker.pose.position.y = self.goalY
+        marker.pose.position.z = 0
+
+        markerArray.markers.append(marker)
+
+        self.publisher.publish(markerArray)
+
+        markerArray2 = MarkerArray()
+        marker2 = Marker()
+        marker2.header.frame_id = "base_link"
+        marker2.type = marker.CUBE
+        marker2.action = marker.ADD
+        marker2.scale.x = abs(act[0])
+        marker2.scale.y = 0.1
+        marker2.scale.z = 0.01
+        marker2.color.a = 1.0
+        marker2.color.r = 1.0
+        marker2.color.g = 0.0
+        marker2.color.b = 0.0
+        marker2.pose.orientation.w = 1.0
+        marker2.pose.position.x = 5
+        marker2.pose.position.y = 0
+        marker2.pose.position.z = 0
+
+        markerArray2.markers.append(marker2)
+        self.publisher2.publish(markerArray2)
+
+        markerArray3 = MarkerArray()
+        marker3 = Marker()
+        marker3.header.frame_id = "base_link"
+        marker3.type = marker.CUBE
+        marker3.action = marker.ADD
+        marker3.scale.x = abs(act[1])
+        marker3.scale.y = 0.1
+        marker3.scale.z = 0.01
+        marker3.color.a = 1.0
+        marker3.color.r = 1.0
+        marker3.color.g = 0.0
+        marker3.color.b = 0.0
+        marker3.pose.orientation.w = 1.0
+        marker3.pose.position.x = 5
+        marker3.pose.position.y = 0.2
+        marker3.pose.position.z = 0
+
+        markerArray3.markers.append(marker3)
+        self.publisher3.publish(markerArray3)
+
+        markerArray4 = MarkerArray()
+        marker4 = Marker()
+        marker4.header.frame_id = "base_link"
+        marker4.type = marker.CUBE
+        marker4.action = marker.ADD
+        marker4.scale.x = 0.1  # abs(act2)
+        marker4.scale.y = 0.1
+        marker4.scale.z = 0.01
+        marker4.color.a = 1.0
+        marker4.color.r = 1.0
+        marker4.color.g = 0.0
+        marker4.color.b = 0.0
+        marker4.pose.orientation.w = 1.0
+        marker4.pose.position.x = 5
+        marker4.pose.position.y = 0.4
+        marker4.pose.position.z = 0
+
+        markerArray4.markers.append(marker4)
+        self.publisher4.publish(markerArray4)
+
+        # reward = act[0]*0.7-abs(act[1])
+        r1 = 1 - 2 * math.sqrt(abs(beta2 / np.pi))
+        r2 = self.distOld - Dist
+        r3 = lambda x: 1-x if x < 1 else 0.0
+        #rl = 0
+        #for r in range(len(laser_state[0])):
+        #    rl += r3(laser_state[0][r])
+        #reward = 0.8 * r1 + 30 * r2 + act[0]/2 - abs(act[1])/2 - r3(min(laser_state[0]))/2
+        reward = act[0] / 2 - abs(act[1]) / 2 - r3(min(laser_state[0])) / 2
+        #reward = 30 * r2 + act[0] / 2 - abs(act[1]) / 2  # - r3(min(laser_state[0]))/2
+        #reward = 0.8 * r1 + 30 * r2
+        #print("r1: ", 0.4*r1, " r2: ", 15*r2, " vr: ", act[0], " r3: ", r3(min(laser_state[0])))
+        self.distOld = Dist
+        #reward = act[0] * 0.7 - abs(act[1])
+
+        if Dist < 0.3:# and act[0] > 0:
+            target = True
+            done = True
+            #self.change_goal()
+            self.distOld = math.sqrt(math.pow(self.odomX - self.goalX, 2) + math.pow(self.odomY - self.goalY, 2))
+            reward = 80
+
+        if col:
+            reward = -100
+
+        #Dist = min(Dist, 5.0)
+        #toGoal = [Dist / 5.0, (beta2 + np.pi) / (np.pi * 2)]
+        toGoal = [Dist, beta2, act[0], act[1]]
+        state = np.append(laser_state, toGoal)
+        return state, reward, done, target  # {}
+
+    def reset(self):
+        # print ("Reset!")
+
+        # Resets the state of the environment and returns an initial observation.
+        rospy.wait_for_service('/gazebo/reset_world')
+        try:
+            self.reset_proxy()
+
+        except rospy.ServiceException as e:
+            print("/gazebo/reset_simulation service call failed")
+
+        # print("set Reset!")
+        angle = np.random.uniform(-np.pi, np.pi)
+        quaternion = Quaternion.from_euler(0., 0., angle)
+        object_state = self.set_self_state
+
+        x = 0
+        y = 0
+        chk = False
+        while not chk:
+            x = np.random.uniform(-4.5, 4.5)
+            y = np.random.uniform(-4.5, 4.5)
+            chk = check_pos(x, y)
+        object_state.pose.position.x = x
+        object_state.pose.position.y = y
+        # object_state.pose.position.z = 0.
+        object_state.pose.orientation.x = quaternion.x
+        object_state.pose.orientation.y = quaternion.y
+        object_state.pose.orientation.z = quaternion.z
+        object_state.pose.orientation.w = quaternion.w
+        self.set_state.publish(object_state)
+
+        self.odomX = object_state.pose.position.x
+        self.odomY = object_state.pose.position.y
+
+        self.change_goal()
+        self.random_box()
+
+        self.distOld = math.sqrt(math.pow(self.odomX - self.goalX, 2) + math.pow(self.odomY - self.goalY, 2))
+
+        data = None
+        # print('wait on laser now')
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            self.unpause()
+        except (rospy.ServiceException) as e:
+            print("/gazebo/unpause_physics service call failed")
+        while data is None:
+            try:
+                data = rospy.wait_for_message('/r1/front_laser/scan', LaserScan, timeout=0.5)
+            except:
+                pass
+        laser_state = np.array(data.ranges[:])
+        laser_state[laser_state == inf] = 10
+        laser_state = binning(0, laser_state, 20)
+        #laser_state = laser_state / 10
+
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            self.pause()
+        except (rospy.ServiceException) as e:
+            print("/gazebo/pause_physics service call failed")
+
+        Dist = math.sqrt(math.pow(self.odomX - self.goalX, 2) + math.pow(self.odomY - self.goalY, 2))
+
+        skewX = self.goalX - self.odomX
+        skewY = self.goalY - self.odomY
+
+        dot = skewX * 1 + skewY * 0
+        mag1 = math.sqrt(math.pow(skewX, 2) + math.pow(skewY, 2))
+        mag2 = math.sqrt(math.pow(1, 2) + math.pow(0, 2))
+        beta = math.acos(dot / (mag1 * mag2))
+
+        if skewY < 0:
+            if skewX < 0:
+                beta = -beta
+            else:
+                beta = 0 - beta
+        beta2 = (beta - angle)
+
+        if beta2 > np.pi:
+            beta2 = np.pi - beta2
+            beta2 = -np.pi - beta2
+        if beta2 < -np.pi:
+            beta2 = -np.pi - beta2
+            beta2 = np.pi - beta2
+
+        #toGoal = [Dist / 5, (beta2 + np.pi) / (np.pi * 2)]
+        toGoal = [Dist, beta2, 0.0, 0.0]
+        state = np.append(laser_state, toGoal)
+        return state
+        # Implemented in subclass
+        # raise NotImplementedError
+
+    def change_goal(self):
+        if self.upper < 10:
+            self.upper += 0.004
+        if self.lower > -10:
+            self.lower -= 0.004
+
+        gOK = False
+
+        while not gOK:
+            self.goalX = self.odomX + random.uniform(self.upper, self.lower)
+            self.goalY = self.odomY + random.uniform(self.upper, self.lower)
+            gOK = check_pos(self.goalX, self.goalY)
+
+
+
+    def random_box(self):
+        for i in range(4):
+            name = 'cardboard_box_' + str(i)
+
+            x = 0
+            y = 0
+            chk = False
+            while not chk:
+                x = np.random.uniform(-6, 6)
+                y = np.random.uniform(-6, 6)
+                chk = check_pos(x, y)
+                d1 = math.sqrt((x-self.odomX)**2 + (y-self.odomY)**2)
+                d2 = math.sqrt((x-self.goalX)**2 + (y-self.goalY)**2)
+                if d1 < 1.5 or d2 < 1.5:
+                    chk = False
+            box_state = ModelState()
+            box_state.model_name = name
+            box_state.pose.position.x = x
+            box_state.pose.position.y = y
+            box_state.pose.position.z = 0.
+            box_state.pose.orientation.x = 0.0
+            box_state.pose.orientation.y = 0.0
+            box_state.pose.orientation.z = 0.0
+            box_state.pose.orientation.w = 1.0
+            self.set_state.publish(box_state)
